7b8bd0f043433f138c46df89e8730fd21c008950
equanimity-23 <70869585+equanimity-23@users.noreply.github.com>
2022-01-31 15:32:08 +1000

    Update Coxme-Survival-Analysis-Template.R

--- Coxme-Survival-Analysis-Template.R
+++ Coxme-Survival-Analysis-Template.R
@@ -1,22 +1,24 @@
 # Template for survival analysis with Cox mixed effects model.
+# Setting up the analysis ----
+install.packages("dplyr") #Installs the dplyr program for data cleanup
+install.packages("coxme") #Installs the Cox Mixed Effect Model
+install.packages("survminer") #Installs the survminer program for ggplot survival curves plotting
 
-#Installs the dplyr program for data cleanup
-install.packages("dplyr")
 library(dplyr)
-
-#Installs the Cox Mixed Effect Model and downloads it into the function library 
-install.packages("coxme")
 library(coxme)
-
-#Installs the survminer program for ggplot survival curves plotting.
-install.packages("survminer")
 library(survminer)
 
-#Renames the data files, input file name here 
+#Renames the data files, replace placeholder with csv file name 
 
 DCV <- placeholder
 
 FHV <- placeholder
+
+#Filters out the PBS samples to stop them from confounding the analysis
+
+DCV_noPBS <- filter(DCV, Virus == 1)
+
+FHV_noPBS <- filter(FHV, Virus == 1)
 
 #test for proportional hazards assumption
 #coxph assumes that hazard ratios remain the same over time
@@ -25,15 +27,11 @@ FHV <- placeholder
 
 fitCoxphDCV <- coxph(Surv(Days, Survival)~ Mutant+Virus, data = DCV_noPBS)
 ftestDCV <- cox.zph(fitCoxphDCV)
+ftestDCV
 
 fitCoxphFHV <- coxph(Surv(Days, Survival)~ Mutant+Virus, data = FHV_noPBS)
 ftestFHV <- cox.zph(fitCoxphFHV)
-
-#Filters out the PBS samples to stop them from confounding the analysis
-
-DCV_noPBS <- filter(DCV, Virus == 1)
-
-FHV_noPBS <- filter(FHV, Virus == 1)
+ftestFHV
 
 #Fits the coxme to the survival data. 
 #Where the fixed effects Virus and the random effects 
@@ -59,8 +57,6 @@ fitFHV
 #fitmiRNA_Virus2 <- coxme(Surv(Days, Survival)~ Mutant*Virus + (1|BiolRep/TechRep), data=FHV)
 #fitmiRNA_Virus2
 
-
-
 #Fits survival curves based on the survival data
 #Wild-type vs mutants
 miRNAcurves <- survfit(Surv(Days, Survival) ~ Mutant, data=DCV_noPBS)
@@ -68,7 +64,7 @@ ggsurvplot(miRNAcurves,
            data = DCV_noPBS,               #change to the virus under analysis
            axes.offset = FALSE,            #changes axes to start at zero
            line = c(1,1),
-           pval = 0.066,                   #change to coxme pvalue
+           pval = placeholder,                   #change to coxme pvalue
            xlab = "Time in days",
            ylab = "Proportional survival",
            break.time.by = 1, 
