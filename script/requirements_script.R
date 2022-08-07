0c539322e204b743caf5192dbd11b7c246d57d7e
equanimity-23 <danielchewie@gmail.com>
2022-08-08 09:47:22 +1000

    - new parametric script

--- script/requirements_script.R
+++ script/requirements_script.R
@@ -1,6 +1,6 @@
 # Package installer script for Coxme analysis
 # Setting up the analysis ----
-install.packages("dplyr") #Installs the dplyr program for data cleanup
+install.packages("tidyverse") #Installs the tidyverse program for data cleanup
 install.packages("coxme") #Installs the Cox Mixed Effect Model
 install.packages("survminer") #Installs the survminer program for ggplot survival curves plotting
 install.packages("here") #Installs the here package for management of file directories. 
